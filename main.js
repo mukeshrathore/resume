c8e4d1f7f3df57407d587b5b3c21f7b605139ca2
mukeshrathore <mukesh.rathore86@gmail.com>
2018-06-24 02:06:04 -0400

    resume attached resume pdf

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var startDay = new Date(2009, 0);
 var today = new Date();
 document.getElementById("yearOfExperience").innerHTML = Math.floor(Math.abs(today.getTime() - startDay.getTime()) / (1000 * 3600 * 24 * 365));
+
+function downloadPDF() {
+    window.open('assets/Mukesh_Rathore_BE_Wells_Fargo_Senior_UI_Developer.pdf');
+}
